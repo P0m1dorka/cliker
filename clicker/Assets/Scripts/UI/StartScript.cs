2a5e75a39e75186a2b6bfdd3991d39c5f2fafd60
sl3d <81529806+P0m1dorka@users.noreply.github.com>
2023-05-13 21:50:28 +0300

    add first scenen

---
+++ clicker/Assets/Scripts/UI/StartScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartScript : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("MainLVL");
+        }
+    }
+    
+}
