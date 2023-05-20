299865296ed710de9319e81bc5883ad039ddd1e1
sl3d <81529806+P0m1dorka@users.noreply.github.com>
2023-05-20 09:36:50 +0300

    .

---
+++ clicker/Assets/Scripts/Tourment/Tourment1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tourment1 : MonoBehaviour
+{
+    [SerializeField] private Button _button;
+    private float _random;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _button.onClick.AddListener()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
