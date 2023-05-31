d7d8410a94fe801a8d16688db4ed2f81ebd41a6c
sl3d <81529806+P0m1dorka@users.noreply.github.com>
2023-05-31 16:50:20 +0300

    done new train like dance sa

---
+++ clicker/Assets/Scripts/Train/SecondTrain/RandomKey.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RandomKey : MonoBehaviour
+{
+    [SerializeField] private KeyCode[] _keyArray = new KeyCode[10];
+    [SerializeField] private TMP_Text _text;
+    [SerializeField] private float _time;
+    [SerializeField] private int _popadanie;
+    System.Random rnd = new System.Random();
+    private bool _input;
+    private int _random;
+    void Start()
+    {
+        StartCoroutine(RandomKeyCode());
+    }
+    private IEnumerator RandomKeyCode()
+    {
+        Coroutine coroutine = null ;
+        while(true)
+        {
+            _random = rnd.Next(0, 10);
+            _text.text = $"{_keyArray[_random]}";
+            coroutine = StartCoroutine(Check(_keyArray[_random]));
+            yield return new WaitForSeconds(_time);
+            StopCoroutine(coroutine);
+            _input = false;
+            _text.color = Color.white;
+        }
+    }
+    private IEnumerator Check(KeyCode _key)
+    {
+        while (!_input)
+        {
+            if (Input.GetKeyUp(_key))
+            {
+                Debug.Log("Popal");
+                _popadanie++;
+                _text.color = Color.green;
+                _input = true;
+
+            }
+            else
+            {
+                _text.color = Color.red;
+                Debug.Log("Ne popal");
+            }
+            yield return new WaitForFixedUpdate();
+        }
+            
+    }
+}
