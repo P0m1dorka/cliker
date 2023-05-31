0baead7c8961df391038b3453916652d4c71848f
sl3d <81529806+P0m1dorka@users.noreply.github.com>
2023-05-31 15:52:02 +0300

    add timer script and done target train

---
+++ clicker/Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float _maxTime;
+    [SerializeField] private string _exitScene;
+    [SerializeField] private TMP_Text _text;
+    void Update()
+    {
+        _text.text = $"Time: {Math.Round((double)_maxTime)}";
+        _maxTime -= Time.deltaTime;
+        if(_maxTime <= 0)
+        {
+            SceneManager.LoadScene(_exitScene);
+        }
+    }
+}
